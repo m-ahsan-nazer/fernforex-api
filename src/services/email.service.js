cbeac00115bb946e8ae72161b39d87fe298ad38f
ahsan <m.ahsan.nazer@gmail.com>
2021-06-09 05:50:27 +1200

    Email services now work.
    Setup a sendgrid account which in turn uses Google cloud app for
    verifying site ownership.

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -19,7 +19,8 @@ if (config.env !== 'test') {
  * @returns {Promise}
  */
 const sendEmail = async (to, subject, text) => {
-  const msg = { from: config.email.from, to, subject, text };
+  // const msg = { from: config.email.from, to, subject, text };
+  const msg = { from: config.email.from, to, subject, html: text };
   await transport.sendMail(msg);
 };
 
@@ -31,11 +32,10 @@ const sendEmail = async (to, subject, text) => {
  */
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
+  const resetPasswordUrl = `www.fernforex.co.nz/reset-password?token=${token}`;
+  const text = `<b>Kia ora</b>,
+<p>To reset your password, click on this link: <a href=${resetPasswordUrl}>reset FernForex 
+account password</a>.</p><p>If you did not request any password resets, then ignore this email.</p>`;
   await sendEmail(to, subject, text);
 };
 
@@ -47,11 +47,11 @@ If you did not request any password resets, then ignore this email.`;
  */
 const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  const verificationEmailUrl = `www.fernforex.co.nz/verify-email?token=${token}`;
+  const text = `<b>Kia ora</b>,
+<p>To verify your email, click on this link: <a href=${verificationEmailUrl}>verify FernForex 
+account</a>.</p><p>If you did not create an account, then ignore this email.</p>`;
+
   await sendEmail(to, subject, text);
 };
 
